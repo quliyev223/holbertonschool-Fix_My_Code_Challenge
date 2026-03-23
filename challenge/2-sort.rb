95df3d5ce377049dd4a8afcbc66f1c8910cf4ea6
quliyev223 <mikayilquliev@gmail.com>
2026-03-23 22:05:56 +0400

    do it now
    Fix insertion logic to correctly place integer in sorted order.

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -5,12 +5,8 @@
 ###
 result = []
 ARGV.each do |arg|
-    # skip if not integer
     next if arg !~ /^-?[0-9]+$/
-    # convert to integer
     i_arg = arg.to_i
-    
-    # insert result at the right position
     is_inserted = false
     i = 0
     l = result.size
@@ -18,7 +14,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i, i_arg) # Fix: use i instead of i - 1
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
