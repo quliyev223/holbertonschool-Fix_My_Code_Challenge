2363d6542f2cd3f072ea768927980a88699fbd98
quliyev223 <mikayilquliev@gmail.com>
2026-03-23 21:40:34 +0400

    Fix FizzBuzz condition order in fizzbuzz.py
    Reorder conditions to check for FizzBuzz before Fizz.

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -16,11 +16,14 @@ def fizzbuzz(n):
         return
 
     tmp_result = []
+# Check for FizzBuzz first (multiples of both 3 and 5) before checking
+# individually, otherwise the first condition would always catch multiples
+# of 15 and FizzBuzz would never be reached.
     for i in range(1, n + 1):
-        if (i % 3) == 0:
-            tmp_result.append("Fizz")
-        elif (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
