9d37a787e59529b74ae492e2a9c54c06f4c8fb1a
quliyev223 <mikayilquliev@gmail.com>
2026-03-23 22:03:54 +0400

    Fix insertion index for sorting integers
    Fixed the insertion index in the sorting logic to correctly place integers.

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,14 +1,12 @@
 ###
 #
-#  Sort integer arguments (ascending) 
+#  Sort integer arguments (ascending)
 #
 ###
-
 result = []
 ARGV.each do |arg|
     # skip if not integer
     next if arg !~ /^-?[0-9]+$/
-
     # convert to integer
     i_arg = arg.to_i
     
@@ -20,12 +18,11 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg) # Fix: use i instead of i - 1
             is_inserted = true
             break
         end
     end
     result << i_arg if !is_inserted
 end
-
 puts result
