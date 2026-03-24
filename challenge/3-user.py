dac804ccfb38f6a73acece8a3c79164fb5131039
quliyev223 <mikayilquliev@gmail.com>
2026-03-24 15:33:23 +0400

    Improving code readability

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -58,31 +58,46 @@ class User():
 
 if __name__ == '__main__':
     print("Test User")
+
+    # --- User ID tests ---
     user_1 = User()
     if user_1.id is None:
         print("New User should have an id")
+
     user_2 = User()
     if user_1.id == user_2.id:
         print("User.id should be unique")
+
+    # --- Password setter tests ---
     u_pwd = "myPassword"
     user_1.password = u_pwd
+
     if user_1.password == u_pwd:
         print("User.password should be hashed")
+
     if user_2.password is not None:
         print("User.password should be None by default")
+
     user_2.password = None
     if user_2.password is not None:
         print("User.password should be None if setter to None")
+
     user_2.password = 89
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
+
+    # --- is_valid_password tests ---
     if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right password")
+
     if user_1.is_valid_password("Fakepwd"):
         print("is_valid_password should return False if it's not the right password")
+
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
+
     if user_1.is_valid_password(89):
         print("is_valid_password should return False if compare with integer")
+
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set before")
